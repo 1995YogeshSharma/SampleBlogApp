662413e9736b215e2b72953e65c983067787932c
1995YogeshSharma <yogesh.sharma@research.iiit.ac.in>
2017-01-19 18:05:21 +0530

    models - model for storing blogs added

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    created_on = models.DateTimeField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.title + "  -  " + self.author.username
